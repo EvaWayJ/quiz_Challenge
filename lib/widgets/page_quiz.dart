73d38a095454054c22f920becb227d7db1b76418
EvaWayJ <ezzeribieva@yahoo.fr>
2020-05-23 15:29:03 +0200

    Création de l'alertdialog et finalisation du projet

--- lib/widgets/page_quiz.dart
+++ lib/widgets/page_quiz.dart
@@ -87,31 +87,58 @@ class _PageQuizState extends State<PageQuiz>{
       score++;
     }
     return showDialog(
-      context: context,
-    barrierDismissible: false,
-      builder: (BuildContext context){
-        return new SimpleDialog(
-          title: new CustomText((bonneReponse)? "C'est gagné!!!!" : "Oups perdu..."),
-          contentPadding: EdgeInsets.all(20.0),
-          children: <Widget>[
-            new Image.asset((bonneReponse)? vrai : faux, fit :BoxFit.cover),
-            new Container(height: 25.0),
-            new CustomText(question.explication, factor: 1.25, color: Colors.grey[900]),
-            new Container(height: 25.0),
-            new RaisedButton(onPressed: () {
-              Navigator.pop(context);
-              questionSuivante();
-            },
-            child: new CustomText("Au suivant", factor: 1.25),
-              color: Colors.blue,
-            )
-          ],
-        );
-      }
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context){
+          return new SimpleDialog(
+            title: new CustomText((bonneReponse)? "C'est gagné!!!!" : "Oups perdu..."),
+            contentPadding: EdgeInsets.all(20.0),
+            children: <Widget>[
+              new Image.asset((bonneReponse)? vrai : faux, fit :BoxFit.cover),
+              new Container(height: 25.0),
+              new CustomText(question.explication, factor: 1.25, color: Colors.grey[900]),
+              new Container(height: 25.0),
+              new RaisedButton(onPressed: () {
+                Navigator.pop(context);
+                questionSuivante();
+              },
+                child: new CustomText("Au suivant", factor: 1.25),
+                color: Colors.blue,
+              )
+            ],
+          );
+        }
     );
   }
+  Future<Null> alert() async{
+    return showDialog(context: context,
+        barrierDismissible: false,
+        builder:( BuildContext buildeContext){
+          return new AlertDialog(
+            title: new CustomText("C'est fini", color: Colors.blue, factor: 1.25,),
+            contentPadding: EdgeInsets.all(10.0),
+            content: new CustomText("Votre score: $score / $index", color: Colors.grey[900]),
+            actions: <Widget>[
+              new FlatButton(
+                  onPressed: (() {
+                    Navigator.pop(buildeContext);
+                    Navigator.pop(context);
+                  }),
+                  child: new CustomText("OK", factor: 1.25, color: Colors.blue))
+            ],
+          );
+        }
+    );
 
+  }
   void questionSuivante(){
-
+    if(index < listeQuestion.length-1){
+      index++;
+      setState(() {
+        question = listeQuestion[index];
+      });
+    }else{
+      alert();
+    }
   }
 }
