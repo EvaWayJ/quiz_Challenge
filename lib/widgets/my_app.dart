7546fced727c84ae999e44d3ba2326cfb3d3c298
EvaWayJ <ezzeribieva@yahoo.fr>
2020-05-28 15:22:04 +0200

    Plateforme Ios ?

--- lib/widgets/my_app.dart
+++ lib/widgets/my_app.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:quizchallenge/widgets/home.dart';
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    if(Platform.isIOS){
+      print("Je suis sur Ios");
+    }
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
