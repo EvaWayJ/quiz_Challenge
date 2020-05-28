93c7d61b4a72b55b72243fd57b009e088df75d3a
EvaWayJ <ezzeribieva@yahoo.fr>
2020-05-28 15:28:32 +0200

    La cupertinoScafold

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:quizchallenge/widgets/custom_text.dart';
 import 'package:quizchallenge/widgets/page_quiz.dart';
@@ -14,37 +17,46 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return (Platform.isIOS)
+    ? new CupertinoPageScaffold(
+        navigationBar: new CupertinoNavigationBar(
+        ),
+        child: body())
+        :Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            new Card(
-              elevation: 10.0,
-              child: new Container(
+      body: body() // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget body (){
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          new Card(
+            elevation: 10.0,
+            child: new Container(
                 height: MediaQuery.of(context).size.width * 0.8,
                 width: MediaQuery.of(context).size.width * 0.8,
                 child: new Image.asset("quiz assets/cover.jpg",
-                fit: BoxFit.cover
+                    fit: BoxFit.cover
                 )
-              ),
             ),
-            new RaisedButton(
+          ),
+          new RaisedButton(
               color: Colors.blue,
-                onPressed: () {
-                  Navigator.push(context,
-                      new MaterialPageRoute(builder: (BuildContext context) {
-                        return new PageQuiz();
-                      }));
-                },
-            child: new CustomText("Commencer le quiz", factor: 1.5)
-            )
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+              onPressed: () {
+                Navigator.push(context,
+                    new MaterialPageRoute(builder: (BuildContext context) {
+                      return new PageQuiz();
+                    }));
+              },
+              child: new CustomText("Commencer le quiz", factor: 1.5)
+          )
+        ],
+      ),
     );
   }
 }
